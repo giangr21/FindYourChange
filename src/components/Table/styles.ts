bd4837030741d1902c84b7e28ea8507f96a013ee
Gian <giangr21@gmail.com>
2021-03-09 19:54:00 -0300

    fix: change table clerk

--- src/components/Table/styles.ts
+++ src/components/Table/styles.ts
@@ -9,7 +9,6 @@ import styled from 'styled-components';
 export const MoreContainer = styled.div`
     display: flex;
     align-items: center;
-    /* justify-content: flex-end; */
     margin: auto 0;
 
     button {
