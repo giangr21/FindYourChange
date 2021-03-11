11b254280f04b0cd9e77ef4d887e03bcb0606b0a
Gustavo <gustavosvargas1998@hotmail.com>
2021-03-11 20:13:59 -0300

    CRUD: Services

--- src/pages/ConfigServicesProvider/ProviderRow/index.tsx
+++ src/pages/ConfigServicesProvider/ProviderRow/index.tsx
@@ -1,18 +1,17 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React, { useCallback } from 'react';
-import { MdEdit, MdDeleteForever, MdRemoveRedEye } from 'react-icons/md';
+import { MdEdit, MdDeleteForever } from 'react-icons/md';
 
 import { Container, MoreContainer } from './styles';
 
 interface ProviderRow {
     handleDelete: (id: string) => void;
     handleEdit: (id: string) => void;
-    handleView: (id: string) => void;
     data: any;
 }
 
-const ProviderRow: React.FC<ProviderRow> = ({ data, handleDelete, handleEdit, handleView }) => {
+const ProviderRow: React.FC<ProviderRow> = ({ data, handleDelete, handleEdit }) => {
     const deleteRow = useCallback(() => {
         handleDelete(data.id);
     }, [data.id, handleDelete]);
@@ -21,20 +20,13 @@ const ProviderRow: React.FC<ProviderRow> = ({ data, handleDelete, handleEdit, ha
         handleEdit(data.id);
     }, [data.id, handleEdit]);
 
-    const viewRow = useCallback(() => {
-        handleView(data.id);
-    }, [data.id, handleView]);
-
     return (
         <Container>
-            <small onClick={viewRow}>{data.title}</small>
-            <small onClick={viewRow}>{data.value}</small>
-            <small onClick={viewRow}>{data.description}</small>
+            <small onClick={editRow}>{data.title}</small>
+            <small onClick={editRow}>{data.value}</small>
+            <small onClick={editRow}>{data.description}</small>
 
             <MoreContainer>
-                <button onClick={viewRow} type="button">
-                    <MdRemoveRedEye color="#8E5BE8" size={20} />
-                </button>
                 <button onClick={editRow} type="button">
                     <MdEdit color="#ffa048" size={20} />
                 </button>
