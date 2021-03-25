d3cac7a11f02a7679b314cf27cf8a01469679029
Gian <giangr21@gmail.com>
2021-03-25 20:41:06 -0300

    refactor: layout product

--- src/pages/Marketplace/index.tsx
+++ src/pages/Marketplace/index.tsx
@@ -4,17 +4,25 @@ import { FaArrowRight, FaSearch } from 'react-icons/fa';
 import { FormHandles } from '@unform/core';
 import { toast } from 'react-toastify';
 import { useHistory } from 'react-router-dom';
+import { withStyle } from 'baseui';
 import IconButton from '../../components/Button/IconButton';
 import Select from '../../components/Select/MainSearchSelect';
 import {
-    Container,
     Content,
     SearchContainer,
     ContentSearch,
     Results,
     ContentResults,
     HeaderResults,
-    Product,
+    ProductCardWrapper,
+    ProductImageWrapper,
+    ProductInfo,
+    Image,
+    ProductTitle,
+    ProductWeight,
+    ProductMeta,
+    ProductPriceWrapper,
+    ProductPrice,
 } from './styles';
 import Radio from '../../components/Radio';
 import Input from '../../components/Input/MainSearchInput';
@@ -22,6 +30,24 @@ import { useAuth } from '../../hooks/Auth';
 import { ProductData } from '../ConfigProductsProvider';
 import api from '../../services/api';
 import Loading from '../../components/Loading';
+import { Row as Rows, Col as Column } from '../../components/FlexBox/FlexBox';
+
+export const Col = withStyle(Column, () => ({
+    marginBottom: '20px',
+
+    '@media only screen and (max-width: 767px)': {
+        ':last-child': {
+            marginBottom: 0,
+        },
+    },
+}));
+
+const Row = withStyle(Rows, () => ({
+    paddingRight: '5px',
+    '@media only screen and (min-width: 768px) and (max-width: 991px)': {
+        alignItems: 'center',
+    },
+}));
 
 const Index: React.FC = () => {
     const { user } = useAuth();
@@ -82,140 +108,175 @@ const Index: React.FC = () => {
     );
 
     return (
-        <Container>
-            <Content>
-                <SearchContainer>
-                    <ContentSearch ref={formRef} onSubmit={() => {}}>
-                        <p>MarketPlace</p>
-                        <Input name="email" icon={FaSearch} placeholder="Pesquisar Marketplace" />
-                        <div className="separator" />
-                        <span>Cidades: </span>
-                        <div
+        <Content>
+            <SearchContainer>
+                <ContentSearch ref={formRef} onSubmit={() => {}}>
+                    <p>MarketPlace</p>
+                    <Input name="email" icon={FaSearch} placeholder="Pesquisar Marketplace" />
+                    <div className="separator" />
+                    <span>Cidades: </span>
+                    <div
+                        style={{
+                            marginTop: '5px',
+                        }}
+                    >
+                        <Radio
                             style={{
-                                marginTop: '5px',
+                                width: '100%',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                // alignItems: 'center',
+                                // justifyContent: 'center',
                             }}
-                        >
-                            <Radio
-                                style={{
-                                    width: '100%',
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    // alignItems: 'center',
-                                    // justifyContent: 'center',
-                                }}
-                                name="neightborhoods"
-                                options={[
-                                    {
-                                        id: 'false',
-                                        label: 'Todas',
-                                    },
-                                    { id: 's', label: 'Curitiba' },
-                                    { id: 'd', label: 'Sao Paulo' },
-                                    { id: 'true', label: 'Santa Catarina' },
-                                ]}
-                                onChange={() => {}}
-                            />
-                        </div>
-                        <div className="separator" />
-                        <span>Categoria: </span>
-                        <div
+                            name="neightborhoods"
+                            options={[
+                                {
+                                    id: 'false',
+                                    label: 'Todas',
+                                },
+                                { id: 's', label: 'Curitiba' },
+                                { id: 'd', label: 'Sao Paulo' },
+                                { id: 'true', label: 'Santa Catarina' },
+                            ]}
+                            onChange={() => {}}
+                        />
+                    </div>
+                    <div className="separator" />
+                    <span>Categoria: </span>
+                    <div
+                        style={{
+                            marginTop: '5px',
+                        }}
+                    >
+                        <Radio
                             style={{
-                                marginTop: '5px',
+                                width: '100%',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                // alignItems: 'center',
+                                // justifyContent: 'center',
                             }}
-                        >
-                            <Radio
-                                style={{
-                                    width: '100%',
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    // alignItems: 'center',
-                                    // justifyContent: 'center',
-                                }}
-                                name="services"
-                                options={[
-                                    {
-                                        id: 'false',
-                                        label: 'Todas',
-                                    },
-                                    { id: 'd', label: 'Tatuagem' },
-                                    { id: 's', label: 'Piercing' },
-                                    { id: 'a', label: 'Barbearia' },
-                                ]}
-                                onChange={() => {}}
-                            />
-                        </div>
-                        <div className="separator" />
-                        <span>Preço: </span>
-                        <div
+                            name="services"
+                            options={[
+                                {
+                                    id: 'false',
+                                    label: 'Todas',
+                                },
+                                { id: 'd', label: 'Tatuagem' },
+                                { id: 's', label: 'Piercing' },
+                                { id: 'a', label: 'Barbearia' },
+                            ]}
+                            onChange={() => {}}
+                        />
+                    </div>
+                    <div className="separator" />
+                    <span>Preço: </span>
+                    <div
+                        style={{
+                            marginTop: '5px',
+                        }}
+                    >
+                        <Radio
                             style={{
-                                marginTop: '5px',
+                                width: '100%',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                // alignItems: 'center',
+                                // justifyContent: 'center',
                             }}
-                        >
-                            <Radio
-                                style={{
-                                    width: '100%',
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    // alignItems: 'center',
-                                    // justifyContent: 'center',
-                                }}
-                                name="services"
-                                options={[
-                                    {
-                                        id: 'false',
-                                        label: '$50 - $100',
-                                    },
-                                    { id: 'true', label: '$100 - $150' },
-                                    { id: 'as', label: '$150 - $200' },
-                                    { id: 'ds', label: '$200 - $250' },
-                                    { id: 's', label: '$250 +' },
-                                ]}
-                                onChange={() => {}}
-                            />
-                        </div>
-                    </ContentSearch>
-                </SearchContainer>
-                {!loading ? (
-                    <Results ref={secondFormRef} onSubmit={() => {}}>
-                        <HeaderResults>
-                            <span>6 produtos encontados. Exibindo resultados de 1 a 6.</span>
-                            <Select
-                                name="Search"
-                                placeholder="A"
-                                options={[
-                                    { value: 'a', label: 'Relevancia' },
-                                    { value: 'b', label: 'Distancia' },
-                                    { value: 'c', label: 'Menor Preco' },
-                                    { value: 'd', label: 'Maior Preco' },
-                                ]}
-                                defaultValue={{ value: 'a', label: 'Relevancia' }}
-                            />
-                        </HeaderResults>
-                        <ContentResults>
+                            name="services"
+                            options={[
+                                {
+                                    id: 'false',
+                                    label: '$50 - $100',
+                                },
+                                { id: 'true', label: '$100 - $150' },
+                                { id: 'as', label: '$150 - $200' },
+                                { id: 'ds', label: '$200 - $250' },
+                                { id: 's', label: '$250 +' },
+                            ]}
+                            onChange={() => {}}
+                        />
+                    </div>
+                </ContentSearch>
+            </SearchContainer>
+            {!loading ? (
+                <Results ref={secondFormRef} onSubmit={() => {}}>
+                    <HeaderResults>
+                        <span>{products.length} produtos encontados. Exibindo resultados de 1 a 6.</span>
+                        <Select
+                            name="Search"
+                            placeholder="A"
+                            options={[
+                                { value: 'a', label: 'Relevancia' },
+                                { value: 'b', label: 'Distancia' },
+                                { value: 'c', label: 'Menor Preco' },
+                                { value: 'd', label: 'Maior Preco' },
+                            ]}
+                            defaultValue={{ value: 'a', label: 'Relevancia' }}
+                        />
+                    </HeaderResults>
+                    <ContentResults>
+                        <Row>
                             {products.map((product) => (
-                                <Product key={product.id}>
-                                    {product.productImage && (
-                                        <img src={`data:image/png;base64,${product.productImage}`} alt="" />
-                                    )}
-                                    <div className="content">
-                                        <span>{product.name}</span>
-                                        <p>RS {product.value}</p>
-                                        <IconButton
-                                            icon={FaArrowRight}
-                                            title="Informações"
-                                            background="#ff9000"
-                                            action={() => handleClickProduct(product.id)}
-                                        />
-                                    </div>
-                                </Product>
+                                <Col xs={12} sm={6} md={4} lg={4}>
+                                    <ProductCardWrapper className="product-card" key={product.id}>
+                                        <ProductImageWrapper>
+                                            <Image url={product.productImage} className="product-image" />
+                                        </ProductImageWrapper>
+                                        <ProductInfo>
+                                            <div
+                                                style={{
+                                                    height: '60px',
+                                                    maxHeight: '60px',
+                                                    overflowY: 'auto',
+                                                    padding: '5px 0px',
+                                                }}
+                                            >
+                                                <ProductTitle>{product.name}</ProductTitle>
+                                            </div>
+
+                                            <div
+                                                style={{
+                                                    height: '60px',
+                                                    maxHeight: '60px',
+                                                    overflowY: 'auto',
+                                                    padding: '5px 0px',
+                                                }}
+                                            >
+                                                <ProductWeight>{product.description}</ProductWeight>
+                                            </div>
+                                            <ProductMeta>
+                                                <ProductPriceWrapper>
+                                                    <ProductPrice>
+                                                        {Intl.NumberFormat('pt-BR', {
+                                                            style: 'currency',
+                                                            currency: 'BRL',
+                                                        }).format(Number(product.value))}
+                                                    </ProductPrice>
+                                                </ProductPriceWrapper>
+                                                <IconButton
+                                                    icon={FaArrowRight}
+                                                    title="Informações"
+                                                    background="#ff9000"
+                                                    action={() => handleClickProduct(product.id)}
+                                                />
+                                            </ProductMeta>
+                                        </ProductInfo>
+                                    </ProductCardWrapper>
+                                </Col>
                             ))}
-                        </ContentResults>
-                    </Results>
-                ) : (
-                    <Loading />
-                )}
-            </Content>
-        </Container>
+                        </Row>
+                    </ContentResults>
+                </Results>
+            ) : (
+                <Loading
+                    style={{
+                        margin: '0 auto',
+                    }}
+                />
+            )}
+        </Content>
     );
 };
 
--- src/pages/Marketplace/styles.ts
+++ src/pages/Marketplace/styles.ts
@@ -1,17 +1,14 @@
 import { Form as Unform } from '@unform/web';
 import styled from 'styled-components';
-
-export const Container = styled.div`
-    height: 100%;
-    width: 100%;
-    background: #fff;
-`;
+import { styled as styledBaseUi } from 'baseui';
+import Images from '../../components/Image/Image';
 
 export const Content = styled.div`
     display: flex;
     max-width: 1200px;
     margin: 0 auto;
     padding: 10px 0px;
+    height: 100%;
 `;
 
 export const SearchContainer = styled.div`
@@ -21,18 +18,17 @@ export const SearchContainer = styled.div`
     height: 100%;
     width: 300px;
     max-width: 300px;
-    background: #f3f4f4;
-    color: #3a3a3a;
-    border: 1px solid #3a3a3a;
+    background: #28262e;
+    color: #f4ede8;
+    border: 1px solid #f4ede8;
     border-radius: 10px;
 
     padding: 40px 30px;
-    /* flex-direction: column; */
     align-items: center;
     /* justify-content: center; */
 
     p {
-        color: #312e38;
+        color: #f4ede8;
         font-size: 18px;
         font-weight: 500;
         margin-bottom: 20px;
@@ -56,7 +52,7 @@ export const ContentSearch = styled(Unform)`
     }
 
     .radioButton span {
-        color: #3a3a3a !important;
+        color: #f4ede8 !important;
         cursor: pointer !important;
         margin-left: 10px !important;
         margin-right: 20px !important;
@@ -93,58 +89,136 @@ export const ContentSearch = styled(Unform)`
 `;
 
 export const Results = styled(Unform)`
-    background: #f3f4f4;
-    color: #3a3a3a;
+    background: #28262e;
+    color: #f4ede8;
     width: 100%;
+    height: 100%;
     padding: 15px;
     border-radius: 10px;
-`;
 
-export const ContentResults = styled.div`
-    margin-top: 15px;
     display: flex;
-    /* flex-direction: column; */
-    align-items: center;
-    justify-content: center;
+    flex-direction: column;
 `;
 
 export const HeaderResults = styled.div`
     display: flex;
     align-items: center;
+    height: 50px;
     span {
         width: 280%;
     }
 `;
 
-export const Product = styled.div`
-    width: 200px;
-    height: 310px;
-    border: 1px solid black;
-    border-radius: 10px;
-    margin: 0px 20px;
+export const ContentResults = styled.div`
+    flex: auto;
+    margin-top: 15px;
+    overflow: hidden;
+    overflow-y: auto;
+    align-items: center;
+    justify-content: center;
 
-    img {
-        padding: 2px;
-        width: 100%;
-        height: 200px;
+    ::-webkit-scrollbar {
+        width: 5px;
     }
-
-    span {
-        font-weight: 500;
-        margin: 5px 0px;
+    ::-webkit-scrollbar-track {
+        background: #f4ede8;
     }
-
-    p {
-        color: #ff9000;
-        margin-bottom: 5px;
+    ::-webkit-scrollbar-thumb {
+        background-color: #ff9000;
+        border-radius: 20px;
+        border: 3px solid #ff9000;
     }
+`;
 
-    .content {
-        height: 110px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
+export const ProductCardWrapper = styled.div`
+    height: 100%;
+    width: 100%;
+    background-color: #f4ede8;
+    position: relative;
+    cursor: pointer;
+    border-radius: 6px;
+`;
+
+export const ProductImageWrapper = styled.div`
+    height: 180px;
+    padding: 5px;
+    position: relative;
+    border-bottom: 1px solid #3a3a3a;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    @media only screen and (max-width: 767px) {
+        height: 165px;
+    }
+`;
+
+export const Image = styledBaseUi(Images, () => ({
+    maxWidth: '100%',
+    maxHeight: '100%',
+    display: 'inline-block',
+}));
+
+export const ProductInfo = styled.div`
+    padding: 5px 20px 10px;
+    @media only screen and (max-width: 767px) {
+        padding: 15px 20px;
+    }
+`;
+
+export const ProductTitle = styled.h3`
+    font-size: 16px;
+    font-weight: 700;
+    color: #28262e;
+    margin: 0 0 7px 0;
+    min-height: 30px;
+    word-break: break-word;
+
+    @media only screen and (max-width: 767px) {
+        font-size: 14px;
+        font-weight: 700;
+        margin: 0 0 5px 0;
+    }
+`;
+
+export const ProductWeight = styled.span`
+    font-size: 14px;
+    font-weight: 700;
+    color: #3a3a3a;
+    word-break: break-word;
+
+    @media only screen and (max-width: 767px) {
+        fontsize: 12px;
+        fontweight: 700;
+    }
+`;
+
+export const ProductMeta = styled.div`
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    @media only screen and (max-width: 767px) {
+        // minHeight: 32px;
+    }
+`;
+
+export const ProductPriceWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    position: relative;
+`;
+
+export const ProductPrice = styled.span`
+    font-size: 14px;
+    font-weight: 700;
+    color: #ff9000;
+
+    @media only screen and (max-width: 767px) {
+        font-size: 12px;
+        font-weight: 700;
+        margin-right: 5px;
     }
 `;
